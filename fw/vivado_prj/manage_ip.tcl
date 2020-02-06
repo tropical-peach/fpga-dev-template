aad46592a6c58645b0872a69d7a82fff3dbda202
⛆PEACH⛆ <tropical-peach@users.noreply.github.com>
2020-02-06 16:42:31 -0700

    Create manage_ip.tcl

---
+++ fw/vivado_prj/manage_ip.tcl
@@ -0,0 +1,45 @@
+# findFiles
+# basedir - the directory to start looking in
+# pattern - A pattern, as defined by the glob command, that the files must match
+proc findFiles { basedir pattern } {
+
+    # Fix the directory name, this ensures the directory name is in the
+    # native format for the platform and contains a final directory seperator
+    set basedir [string trimright [file join [file normalize $basedir] { }]]
+    set fileList {}
+
+    # Look in the current directory for matching files, -type {f r}
+    # means ony readable normal files are looked at, -nocomplain stops
+    # an error being thrown if the returned list is empty
+    foreach fileName [glob -nocomplain -type {f r} -path $basedir $pattern] {
+        lappend fileList $fileName
+    }
+
+    # Now look for any sub direcories in the current directory
+    foreach dirName [glob -nocomplain -type {d  r} -path $basedir *] {
+        # Recusively call the routine on the sub directory and append any
+        # new files to the results
+        set subDirList [findFiles $dirName $pattern]
+        if { [llength $subDirList] > 0 } {
+            foreach subDirFile $subDirList {
+                lappend fileList $subDirFile
+            }
+        }
+    }
+    return $fileList
+ }
+
+
+# Create managed project area
+create_project -force managed_ip_project ../src/managed_ip  -part $partName
+set_property target_language VERILOG [current_project]
+cd ../src
+# Add IP libraries to project
+add_files [findFiles ./ *.xci] 
+
+#reset output products
+reset_target all [get_ips]
+
+# Generate output products
+generate_target all [ get_ips ]
+
